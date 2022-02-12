ed7a591fb2141d0f5f7a9fadbdb4d6611377bfd5
EdinHg <54362106+EdinHg@users.noreply.github.com>
2022-02-12 21:40:18 +0100

    Add files via upload

--- newfile.py
+++ newfile.py
@@ -6,20 +6,14 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
+from kivy.properties import StringProperty
 from kivy.graphics import Line
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 
+import time
+from datetime import date, timedelta
 
-
-
-#class MyGrid(Widget):
-#    name = ObjectProperty(None)
-#    
-#    def btn(self):
-#        print("kek")
-#    
-#    pass
 
 def nadji_linije(suma):
 	a = 0
@@ -34,52 +28,43 @@ def nadji_linije(suma):
 	return dots
 	
 koor = {
-	"1" : (270, 740+1300),
-	"2" : (540, 740+1300),
-	"3" : (810, 740+1300),
-	"4" : (270, 470+1300),
-	"5" : (540, 470+1300),
-	"6" : (810, 470+1300),
-	"7" : (810, 200+1300),
-	"8" : (540, 200+1300),
-	"9" : (270, 200+1300)
+	"1" : (270, 2040),
+	"2" : (540, 2040),
+	"3" : (810, 2040),
+	"4" : (270, 1770),
+	"5" : (540, 1770),
+	"6" : (810, 1770),
+	"7" : (270, 1500),
+	"8" : (540, 1500),
+	"9" : (810, 1500)
 }
 
-
-suma = "2463867"
-
-
-class MainScreen(Screen):
-    datum = ObjectProperty(None)
-    pass
-    
-
-
 class Output(Screen):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        
-        
-        
-        self.dots = nadji_linije(suma)
-        	
-        
-        
-           
-        with self.canvas:
-            Line(points=[220, 2090, 860, 2090, 860, 1450, 220, 1450, 220, 2090], width=10)
-            Line(points=self.dots, width=7)
     
+    
+    danas = ObjectProperty(date.today())
+    linije = ObjectProperty((270, 1850, 810, 1850, 350, 1500, 540, 2040, 730, 1500, 270, 1850))
+    
+    def calculate(self):
+        datum_rodenja = int((self.ids.datum_rodenja.text).replace(".", ""))
+        suma = str(int((self.danas).strftime("%d.%m.%Y.").replace(".", "")) + datum_rodenja).replace("0", "")
+        self.linije = nadji_linije(suma)
+        
+    def next_day(self):
+        self.danas += timedelta(days = 1) 
+       
+    def previous_day(self):
+        self.danas -= timedelta(days = 1)
+
     
 class WindowManager(ScreenManager):
     pass
     
-kv =  Builder.load_file("my.kv")    
+    
+kv =  Builder.load_file("my.kv")     
+
     
 class MyApp(App):
-    def change(suma):
-            suma += 1
-            return suma 
     def build(self):
         return kv
         
